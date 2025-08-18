dbc68ca20dbe2c275643e05da1cc134e2eb3fbb3
abhi <abhishek>
2025-08-18 19:47:46 +0530

    adding chnages

---
+++ 01_variable.js
@@ -0,0 +1,16 @@
+const accountId=144532
+let accountEmail="abab@.com"
+var accountPassword="12345"
+let accountCity="abc"
+var accountState;
+
+//accountId=6 //not allowed
+
+accountEmail="agag@.com"
+accountPassword="2131"
+accountCity="Lucknow"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountPassword,accountState]);
+   git config --global user.email "abhishek"
+     git config --global user.name "abhi"
---
+++ 02_datatypes.js
@@ -0,0 +1,22 @@
+"use stricrt";// treat all JS code as newer version
+
+// alert (3+3)// we are using node js, not browser
+
+
+let name="hitesh"//string datatype
+let age=13//number
+let isLoggedIn = false//boolean datatypes
+
+// number=> 2 to power 53 
+//bigint
+//string => ""
+//boolean => true/false
+//null=> standlone value
+// undefined =>
+// symbol => uniqe
+ 
+//object
+
+console.log(typeof null);// iska type object aata h .
+//type of undefined is undefined
+
