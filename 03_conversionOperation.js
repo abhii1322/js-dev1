e600a96f787c70e174d1ed1b1b2275ccea6d493a
abhi <abhishek>
2025-08-18 22:59:57 +0530

    Conversion of datatypes

---
+++ 03_conversionOperation.js
@@ -0,0 +1,27 @@
+//let score = "abhi"
+
+//let valueInNumber= Number(score)
+//console.log(typeof valueInNumber);
+//console.log( valueInNumber);
+  
+
+//"33"  => 33
+//"33abs"=> NaN(NOT A NUMBER)
+//True => 1; false=> 0
+
+
+let isLoggedIn ="hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => True; 0 => false
+//""=> false
+//"hitesh" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
