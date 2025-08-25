4971c5ce56743e89e74ad91d090be53308054c3a
abhi <abhishek>
2025-08-26 00:08:19 +0530

    commiting

---
+++ 18-object.js
@@ -0,0 +1,17 @@
+ const course = {
+    coursename: "js in hindi ",
+    price: "9999",
+    courseInstructor:"abhishek"
+}
+
+//course.courseInstructor
+
+const {courseInstructor:instructor}= course
+
+//console.log(courseInstructor);
+console.log(instructor);
+
+
+
+
+
