5dc49fc031008eea6e7c73a96565c4e50c6631d5
abhi <abhishek>
2025-08-26 00:43:38 +0530

    18 0bject .js

--- 18-object.js
+++ 18-object.js
@@ -12,6 +12,15 @@ const {courseInstructor:instructor}= course
 console.log(instructor);
 
 
+// this is json nd isme keys nd value dono string hote h    (Api)
+//{                                          
+   // "name": "hitesh"
+    //"coursename": "js in hindi"
+    //"price": "free"
+//}                                             
 
-
-
+[                             //api ko hm araay me bhi likh skte h 
+    {},
+    {},
+    {}
+]
