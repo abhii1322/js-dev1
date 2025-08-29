bed11d34cac22b05920cd95089165a2fc71c1936
abhi <abhishek>
2025-08-29 13:27:53 +0530

    21- scope in js
    scopes

---
+++ scope in js/21- scope.js
@@ -0,0 +1,21 @@
+//var c= 300
+
+
+let a = 300
+if (true){
+    let a= 10
+    const b = 20 
+console.log("INNER", a);
+
+
+
+}                                  // if curly braces come with function and if else then it is called scope  and  {} scope ke andr jo likhte h use block scope nd bhr vaale ko global scope bolte h
+
+
+console.log(a);
+//console.log(b);
+//console.log(c);
+   
+
+
+// 
