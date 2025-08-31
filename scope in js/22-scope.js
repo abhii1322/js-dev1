993aca36b7403e9c5cc9ba964f7f5b590198ac55
abhi <abhishek>
2025-09-01 03:49:37 +0530

    22-scope in  js

---
+++ scope in js/22-scope.js
@@ -0,0 +1,50 @@
+function one(){
+    const username = "abhishek"
+
+function two(){
+    const website = "youtube"
+    //console.log(username);
+    
+}
+//console.log(website);                
+
+two()
+
+} 
+
+//one()
+
+
+if (true){
+    const username = "Abhishek"
+    if (username==="Abhishek"){
+        const website = " youtube"
+        //console.log(username + website);
+        
+    
+    }
+    //console.log(website); // error
+    
+}
+
+//console.log(username);     error
+
+
+
+//++++++++++++++++++++++++++++++++++++++= interesting +++++++++++++++++++++++++=======
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+ //sec method is trah bhi fuction bnte hue dikte h 
+ addTwo(5)
+ const addTwo = function(num) {
+    return num + 2
+
+ } 
+
