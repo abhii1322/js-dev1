5567660dcaacb1cfd414f51cd488ecfadc5cb216
abhi <abhishek>
2025-09-05 17:06:24 +0530

    Update 26-switch.js

--- control flow in js/26-switch.js
+++ control flow in js/26-switch.js
@@ -1,6 +1,6 @@
 //++++++++++++++++++++++++++++++++++++++++++++++++++++ switch case +++++++++++++++++++++++++++++++++++++++++++++++++++
 
-// switch (key){                   syntax
+// switch (key){                   syntax                   
 //     case value:
 
 //     break;
@@ -8,4 +8,25 @@
 //     default:
 //         break;
 // }
+  
+const month = 3
 
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("feburary");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+
+    default:
+        console.log("default cas match");
+        
+        break;
+}
