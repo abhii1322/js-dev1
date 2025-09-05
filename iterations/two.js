123be07ed54291242e30d517aa7b0803bf3ad3b4
Shikhar Singh <cratesium@gmail.com>
2025-09-06 03:30:26 +0530

    adding message for your problem

---
+++ iterations/two.js
@@ -0,0 +1,27 @@
+first pull 
+
+before any push 
+
+
+since 
+
+i am adding chnages from my laptop 
+
+
+that are not present at your local computer 
+
+ 
+you have to first commit your chnages then  take a pull 
+
+
+then  push the chnages 
+
+
+
+1. commit your chnages 
+2. pull latest chnages from remote 
+3. by pulling latest hcnages from remote what will happens 
+   the chnages made by me will be visaible in your computer 
+   then you can push it
+
+4 push the code to remote    
